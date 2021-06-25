bb2532449a55f539b83d210bfdeb426475515661
Cristian Tomasi <cristian.tomasi@kynetics.com>
2021-06-25 15:15:49 +0200

    Remove TargetTokenFoundListener

--- core/core-api/src/main/kotlin/org/eclipse/hara/ddiclient/core/api/HaraClientData.kt
+++ core/core-api/src/main/kotlin/org/eclipse/hara/ddiclient/core/api/HaraClientData.kt
@@ -20,7 +20,6 @@ data class HaraClientData constructor(
         val serverType: ServerType = ServerType.UPDATE_FACTORY,
         val gatewayToken: String? = null,
         val targetToken: String? = null,
-        val targetTokenFoundListener: TargetTokenFoundListener? = null
 ) {
 
     init {
@@ -30,9 +29,6 @@ data class HaraClientData constructor(
         validUrl(serverUrl, "serverUrl")
         if ((gatewayToken == null || gatewayToken.isBlank()) && (targetToken == null || targetToken.isBlank())) {
             throw IllegalStateException("gatewayToken and targetToken cannot both be empty")
-        }
-        if (targetTokenFoundListener != null && serverType == ServerType.HAWKBIT) {
-            throw IllegalStateException("targetTokenFoundListener can only be used with the UPDATE_FACTORY serverType")
         }
     }
 
